68b7b70ee2ff0471e1c0d162f44d78a6b456ab5b
Ermal Guni <ermalguni@gmail.com>
2025-11-25 18:11:48 +0100

    Add cor

--- src/hiveden/api/server.py
+++ src/hiveden/api/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from hiveden.api.routers import config, docker, info, lxc, shares
 
@@ -8,9 +9,17 @@ app = FastAPI(
     version="0.1.0",
 )
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(config.router)
 app.include_router(docker.router)
 app.include_router(info.router)
 app.include_router(lxc.router)
 app.include_router(shares.router)
-
